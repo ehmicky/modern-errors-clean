b24b47f79549d52621464321aaa5a837cb27f764
ehmicky <ehmicky@gmail.com>
2022-11-04 21:14:27 +0100

    Rename repository

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
-export default function templateName(value) {
+export default function modernErrorsStack(value) {
   return value
 }
--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 import test from 'ava'
-import templateName from 'template-name'
+import modernErrorsStack from 'modern-errors-stack'
 
 test('Dummy test', (t) => {
-  t.true(templateName(true))
+  t.true(modernErrorsStack(true))
 })
