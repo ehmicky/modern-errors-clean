07b847effdd65998520fbe4bd0d42d071ca21a8c
ehmicky <ehmicky@gmail.com>
2022-11-13 15:51:29 +0100

    Fix types

--- types/main.test-d.ts
+++ types/main.test-d.ts
@@ -1,11 +1,18 @@
 import { expectType, expectError } from 'tsd'
 
-import modernErrors from 'modern-errors'
+import ModernError from 'modern-errors'
 import modernErrorsClean from 'modern-errors-clean'
 
-const BaseError = modernErrors([modernErrorsClean])
-const error = new BaseError('', { cause: '' })
+const BaseError = ModernError.subclass('BaseError', {
+  plugins: [modernErrorsClean],
+})
+const error = new BaseError('')
 
-expectError(modernErrors([modernErrorsClean], { stack: undefined }))
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsClean],
+    stack: undefined,
+  }),
+)
 
 expectType<string | undefined>(error.stack)
